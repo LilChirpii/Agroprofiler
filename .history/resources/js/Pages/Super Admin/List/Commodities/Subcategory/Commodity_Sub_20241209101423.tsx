460a125e0c1b06226b5d58156709772e9bec64b8
Kaye Antoinette P. Panaligan <kayeantoinette.panaligan@dssc.edu.ph>
2025-02-10 01:13:00 +0800

    2/10/25

--- .history/resources/js/Pages/Super Admin/List/Commodities/Subcategory/Commodity_Sub_20241209101423.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function Commodity_Sub() {
-    return <div>Commodity_Sub</div>;
-}
--- .history/resources/js/Pages/Super Admin/List/Crop_Damages/CropDamageCauses_20241209143612.tsx
+++
@@ -1,9 +0,0 @@
-const CropDamageCauses = () => {
-  return (
-    <div>
-      <h1>Crop Damage Causes</h1>
-    </div>
-  )
-}
-
-export default CropDamageCauses
